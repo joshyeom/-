87d7379ffc13dfe2fd4a086b5654ff826fc011a5
염정호(Josh) <105287510+joshyeom@users.noreply.github.com>
2023-12-06 22:35:20 +0900

    [Silver III] Title: 1로 만들기, Time: 192 ms, Memory: 17440 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+// TODO: 제출 시 경로 변환 필수 ("/dev/stdin")
+const input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .replaceAll("\r", "")
+  .split("\n");
+
+// 풀이
+function solution(s) {
+  s = Number(s);
+  const DP = new Array(s + 1).fill(0);
+  for (let i = 2; i <= s; i++) {
+    DP[i] = DP[i - 1] + 1;
+
+    if (i % 2 === 0) {
+      DP[i] = Math.min(DP[i], DP[i / 2] + 1);
+    }
+
+    if (i % 3 === 0) {
+      DP[i] = Math.min(DP[i], DP[i / 3] + 1);
+    }
+  }
+  return DP[s];
+}
+
+console.log(solution(input));
