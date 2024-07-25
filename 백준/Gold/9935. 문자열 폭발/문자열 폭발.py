9f8a7c2623b9b7c8c65c5f341d67a3bc8d7b6e1c
염정호(Josh) <105287510+joshyeom@users.noreply.github.com>
2024-07-25 12:18:49 +0900

    [Gold IV] Title: 문자열 폭발, Time: 676 ms, Memory: 42300 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -1,19 +1,25 @@
 import sys
 input = sys.stdin.readline
-str = input().strip()
-bomb = input().strip()
+
+str_string = input().strip()  # 원본 문자열
+bomb_string = input().strip()  # 제거할 부분 문자열
 
 stack = []
 
-for i in range(len(str)):
-    stack.append(str[i])
+# 스택을 사용하여 문자열 처리
+for char in str_string:
+    stack.append(char)
+    
+    # 스택의 끝부분이 bomb 문자열과 일치하는지 확인
+    if "".join(stack[-len(bomb_string):]) == bomb_string:
+        # 부분 문자열이 일치하면 제거
+        del stack[-len(bomb_string):]
 
-    if "".join(stack[-len(bomb):]) == bomb:
-       for _ in range(len(bomb)):
-            stack.pop()
+# 결과 문자열 생성
+result = "".join(stack)
 
-if stack:
-    print("".join(stack))
-
+# 결과 출력
+if result:
+    print(result)
 else:
-   print('FRULA')
+    print("FRULA")
