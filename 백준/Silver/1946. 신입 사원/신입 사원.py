86d50393d0366d621615890180b0d218b3fc2830
염정호(Josh) <105287510+joshyeom@users.noreply.github.com>
2024-07-31 20:33:42 +0900

    [Silver I] Title: 신입 사원, Time: 2392 ms, Memory: 35892 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,24 +1,17 @@
-# 순위를 비교하고 하나의 종목이라도 낫다면 채용
-# 둘다 안되면 불채용
-# 시간 복잡도 O(N)
+import sys
+input = sys.stdin.readline
 
-from sys import stdin
-input = stdin.readline
-N = int(input())
-
-for _ in range(N):
-    M = int(input())
-    numbers = [list(map(int, input().split())) for _ in range(M)]
-    numbers.sort()
-
-    # [[1, 4], [2, 3], [3, 2], [4, 1], [5, 5]]
-    # [[1, 4], [2, 5], [3, 6], [4, 2], [5, 7], [6, 1], [7, 3]]
-
-    count = 1
-    max_ = numbers[0][1]
-    for i in range(1, M):
-        if max_ > numbers[i][1]:
-            count += 1
-            max_ = numbers[i][1]
-
-    print(count)
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    L = [0 for __ in range(n+1)]
+    for i in range(n):
+        a, b = map(int, input().split())
+        L[a]=b
+    min_rank = L[1]
+    cnt = 1
+    for j in range(2, n+1):
+        if min_rank>L[j]:
+            cnt+=1
+            min_rank=L[j]
+    print(cnt)
